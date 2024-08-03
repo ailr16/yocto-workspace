e4d892ddb277b8879509acc73c380ca714f19c2e
ailr16 <angel_ivan_5@outlook.com>
2024-08-03 01:21:21 -0600

    separated dependencies install in multiple lines and clone repositories

--- Linux/install_dependencies.sh
+++ Linux/install_dependencies.sh
@@ -2,6 +2,15 @@
 
 add-apt-repository ppa:deadsnakes/ppa --yes
 
-apt install -y software-properties-common gawk wget  git diffstat unzip texinfo gcc build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev python3-subunit mesa-common-dev zstd liblz4-tool file locales libacl1 nano screen tree
+apt install -y software-properties-common gawk wget  git diffstat unzip texinfo gcc build-essential \
+               chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils \
+               iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev python3-subunit \
+               mesa-common-dev zstd liblz4-tool file locales libacl1 nano screen tree
 
 locale-gen en_US.UTF-8
+
+mkdir ~/yocto
+cd ~/yocto
+git clone git://git.yoctoproject.org/poky -b nanbield
+git clone git://git.openembedded.org/meta-openembedded -b nanbield
+git clone git://git.yoctoproject.org/meta-raspberrypi -b nanbield
