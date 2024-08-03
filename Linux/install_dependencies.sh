04a4b54a5cd9f3172c027dd2e754256419957896
ailr16 <angel_ivan_5@outlook.com>
2024-08-03 01:32:42 -0600

    fixed sudo permissions

--- Linux/install_dependencies.sh
+++ Linux/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-add-apt-repository ppa:deadsnakes/ppa --yes
+sudo add-apt-repository ppa:deadsnakes/ppa --yes
 
-apt install -y software-properties-common gawk wget  git diffstat unzip texinfo gcc build-essential \
+sudo apt install -y software-properties-common gawk wget  git diffstat unzip texinfo gcc build-essential \
                chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils \
                iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev python3-subunit \
                mesa-common-dev zstd liblz4-tool file locales libacl1 nano screen tree
